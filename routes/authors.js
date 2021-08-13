cdfdfd3d6b2e051ccef7af2e57e8b3b33b18c820
samuelAytenfisu <sayten@uw.edu>
2021-08-12 18:29:44 -0700

    final

--- routes/authors.js
+++ routes/authors.js
@@ -1,24 +1,33 @@
 const { Router } = require("express");
 const router = Router();
 
-const authorDAO = require('../daos/author');
+const authorDAO = require("../daos/author");
 
 // Create
 router.post("/", async (req, res, next) => {
   const author = req.body;
-  if (!author || JSON.stringify(author) === '{}' ) {
-    res.status(400).send('author is required');
+  if (!author || JSON.stringify(author) === "{}") {
+    res.status(400).send("author is required");
   } else {
     try {
       const savedAuthor = await authorDAO.create(author);
-      res.json(savedAuthor); 
-    } catch(e) {
+      res.json(savedAuthor);
+    } catch (e) {
       if (e instanceof authorDAO.BadDataError) {
         res.status(400).send(e.message);
       } else {
         res.status(500).send(e.message);
       }
     }
+  }
+});
+
+router.get("/stats", async (req, res, next) => {
+  const author = await authorDAO.getById(req.params.id);
+  if (author) {
+    res.json(author);
+  } else {
+    res.sendStatus(404);
   }
 });
 
@@ -31,7 +40,6 @@ router.get("/:id", async (req, res, next) => {
     res.sendStatus(404);
   }
 });
-
 // Read - all authors
 router.get("/", async (req, res, next) => {
   let { page, perPage } = req.query;
@@ -40,18 +48,17 @@ router.get("/", async (req, res, next) => {
   const authors = await authorDAO.getAll(page, perPage);
   res.json(authors);
 });
-
 // Update
 router.put("/:id", async (req, res, next) => {
   const authorId = req.params.id;
   const author = req.body;
-  if (!author || JSON.stringify(author) === '{}' ) {
+  if (!author || JSON.stringify(author) === "{}") {
     res.status(400).send('author is required"');
   } else {
     try {
       const success = await authorDAO.updateById(authorId, author);
-      res.sendStatus(success ? 200 : 400); 
-    } catch(e) {
+      res.sendStatus(success ? 200 : 400);
+    } catch (e) {
       if (e instanceof authorDAO.BadDataError) {
         res.status(400).send(e.message);
       } else {
@@ -60,16 +67,14 @@ router.put("/:id", async (req, res, next) => {
     }
   }
 });
-
 // Delete
 router.delete("/:id", async (req, res, next) => {
   const authorId = req.params.id;
   try {
     const success = await authorDAO.deleteById(authorId);
     res.sendStatus(success ? 200 : 400);
-  } catch(e) {
+  } catch (e) {
     res.status(500).send(e.message);
   }
 });
-
 module.exports = router;
